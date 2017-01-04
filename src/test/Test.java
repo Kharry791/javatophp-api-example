586161d2a8eef010edf50b7db44795331e58ce79
Harry Kurniawan <k.harry791@gmail.com>
2017-01-04 13:17:50 +0700

    .

--- src/test/Test.java
+++ src/test/Test.java
@@ -8,8 +8,6 @@ package test;
 import com.google.gson.Gson;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
